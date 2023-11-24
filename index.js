daf30ef67d5e281f4812fc36f54a8da043abe00f
Dmitry <dmitry.smetyukhov@gmail.com>
2023-11-24 19:49:50 +0200

    processing of hard operations by workerpool library implementation

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
+const workerpool = require('workerpool');
 const express = require('express');
-const bodyParser = require('body-parser');
-const { Worker } = require('worker_threads');
-const path = require('path');
-
 const app = express();
-app.use(bodyParser.json());
-
-const MAX_CONCURRENT_WORKERS = 1; // Set your desired maximum number of concurrent workers
-
-let activeWorkers = 0;
 
 app.get('/', (req, res) => {
-  if (activeWorkers < MAX_CONCURRENT_WORKERS) {
-    const worker = new Worker(path.join(__dirname, 'hardOperationWorker.js'), {
-      workerData: { coefficient: 1.5 },
-    });
+    const pool = workerpool.pool(__dirname + '/workerPool.js', {maxWorkers: 2});
 
-    activeWorkers++;
-
-    worker.on('message', message => {
-      console.log(`Worker: ${message.result}`);
-      res.json({ sum: message.result });
-    });
-
-    worker.on('error', error => {
-      console.error(`Worker error: ${error}`);
-      res.status(500).json({ error: 'Internal Server Error' });
-    });
-
-    worker.on('exit', code => {
-      activeWorkers--;
-      if (code !== 0) {
-        console.error(`Worker stopped with exit code ${code}`);
-      }
-    });
-  } else {
-    res.status(503).json({ error: 'Service Unavailable. Maximum workers reached.' });
-  }
+    // Pass arguments to the hardOperation function
+    const coeffitient = 1.1;
+    pool.exec('hardOperation', [coeffitient])
+        .then(function (result) {
+            console.log(result)
+            res.send({ result});
+        })
+        .catch(function (err) {
+            console.error(err);
+        })
+        .then(function () {
+            console.log('worker is terminated. work is done!')
+            pool.terminate(); // terminate all workers when done
+        });
 });
 
 app.get('/test', (req, res) => {
-  res.send('TEST');
+    res.send('TEST');
 });
 
 app.listen(3000, () => {
-  console.log('Server started on PORT 3000');
+    console.log("listening on port 3000");
 });
